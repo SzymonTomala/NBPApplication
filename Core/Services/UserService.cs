a5415868e85b342c2f7ad7ae3f4c26bc2f7abc0a
= <szymon.tomala@gmail.com>
2022-02-01 22:57:30 +0100

    user services added

---
+++ Core/Services/UserService.cs
@@ -0,0 +1,54 @@
+﻿using Core.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly CurrencyContext _context;
+
+        public UserService(CurrencyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<decimal> GetCurrentExchangeRate(string currencyCode);
+        {
+            DateTime today = DateTime.Today;
+            var currentCurrencyRate = await _context.CurrencyRates
+                .FirstOrDefaultAsync(x => x.Date == today && x.Code == currencyCode);
+
+            return currentCurrencyRate.Value;
+            
+        }
+
+        public async Task<decimal> GetHistoricalExchangeRate(string currencyCode, DateTime date)
+        {
+            var currentCurrencyRate = await _context.CurrencyRates
+                .FirstOrDefaultAsync(x => x.Date == date && x.Code == currencyCode);
+
+            return currentCurrencyRate.Value;
+        }
+
+        public async Task<decimal> RecalculateCurrencyFromPln(string currencyCode, decimal amount)
+        {
+            var currentCurrencyRate = await _context.CurrencyRates
+                .FirstOrDefaultAsync(x => x.Code == currencyCode);
+
+            return Math.Round(amount / currentCurrencyRate.Value, 2);
+        }
+
+        public async Task<decimal> RecalculateCurrencyToPln(string currencyCode, decimal amount)
+        {
+            var currentCurrencyRate = await _context.CurrencyRates
+                .FirstOrDefaultAsync(x => x.Code == currencyCode);
+
+            return Math.Round(amount * currentCurrencyRate.Value, 2);
+        }
+    }
+}
